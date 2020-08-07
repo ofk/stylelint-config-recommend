4ff9ae7a824fb1ea3cf9d6df4fdd7dcf9daefcf7
ofk <ofkjpn+github@gmail.com>
2020-08-07 16:07:34 +0900

    Add config using stylelint-config-standard

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['stylelint-prettier/recommended'],
+  extends: ['./standard.js', './prettier.js'].map(require.resolve),
 };
---
+++ standard.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // Since stop to update stylelint-config-standard-scss,
+  // get the expected behaivor by loading as follows:
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+};
