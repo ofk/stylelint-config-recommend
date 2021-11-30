2c52099b7eccd7a773a5bb8e953d37871cee496b
ofk <ofkjpn+github@gmail.com>
2021-11-30 11:56:01 +0900

    Upgrade deps and fix config and tests

--- standard.js
+++ standard.js
@@ -2,4 +2,7 @@ module.exports = {
   // Since stop to update stylelint-config-standard-scss,
   // get the expected behaivor by loading as follows:
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+  rules: {
+    'color-function-notation': 'legacy',
+  },
 };
